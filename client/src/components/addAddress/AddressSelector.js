d3b9662865bb52d5d2b23d9c7f781b786a00671a
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-21 22:46:13 +0100

    added auto input component

--- client/src/components/addAddress/AddressSelector.js
+++ client/src/components/addAddress/AddressSelector.js
@@ -6,16 +6,26 @@ const AddressSelector = () => {
     const addressContext = useContext(AddressContext); 
 
     const {
-      addresses
+      addresses,
+      setAddress
     } = addressContext;
+
+    const handleAddressSelect = e => {
+        const addressArray = e.target.value.split(',');
+        setAddress(addressArray);
+      }
 
     if(addresses.length > 0)
     return (
         <div>
             <label htmlFor="address">Choose The Address</label>
-              <input  type="data-list" name="address" list="address-array"></input>
-              <datalist id="address-array">
-                
+              <input onSelect={e => handleAddressSelect(e)} type="data-list" name="address" list="address-array"></input>
+                <datalist id="address-array">
+                    {addresses.map((address) => {
+                    return (
+                        <option value={address} key={address}></option>
+                    );
+                    })}
                 </datalist>
         </div>
     )
---
+++ client/src/components/addAddress/AutoAddress.js
@@ -0,0 +1,155 @@
+import React, { useContext } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import AddressContext from '../../context/address/addressContext';
+
+
+
+const AutoAddress = () => {
+
+
+    //Bringing in context into the component
+    const addressContext = useContext(AddressContext); 
+
+     // Destructuring the context methods I need
+    const { 
+        addContact,
+        name,
+        address1,
+        address2,
+        town,
+        county,
+        postcode,
+        telephone,
+        email,
+
+    } = addressContext;
+
+
+    const onSubmit = (e) => {
+            // stop default form submission
+            e.preventDefault();
+             //creating a contact object which I can then add to the array
+            const createContact = {
+            // I brought in the uuid module for easy id creation
+            id: uuidv4(),
+            name,
+            address1,
+            address2,
+            town,
+            county,
+            postcode,
+            telephone,
+            email,
+            setName,
+            setAddress1,
+            setAddress2,
+            setTown,
+            setCounty,
+            setPostcode,
+            setTelephone,
+            setEmail
+            }
+            addContact(createContact);
+
+            //this was the annoying thing I had to do to keep the local and global states separate 
+            //because of not having a back end in to make get calls to
+
+            setName("");
+            setAddress1("");
+            setAddress2("");
+            setTown("");
+            setCounty("");
+            setPostcode("");
+            setTelephone("");
+            setEmail("");
+
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+                <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    placeholder="Enter Address Name"
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <input
+                    type="text"
+                    name="address1"
+                    value={address1}
+                    placeholder="Enter Address Line 1"
+                    onChange={(e) => setAddress1(e.target.value)}
+                    />
+
+                <input
+                    type="text"
+                    name="address2"
+                    value={address2}
+                    placeholder="Enter Address Line 2"
+                    onChange={(e) => setAddress2(e.target.value)}
+
+                />
+
+                <input
+                    type="text"
+                    name="town"
+                    value={town}
+                    placeholder="Enter Town/City"
+                    onChange={(e) => setTown(e.target.value)}
+
+                />
+
+                <input
+                    type="text"
+                    name="county"
+                    value={county}
+                    placeholder="Enter County"
+                    onChange={(e) => setCounty(e.target.value)}
+
+
+                />
+
+                <input
+                    type="text"
+                    name="postcode"
+                    value={postcode}
+                    placeholder="Enter Postcode"
+                    onChange={(e) => setPostcode(e.target.value)}
+
+                />
+                
+                <input
+                    type="text"
+                    name="telephone"
+                    value={telephone}
+                    placeholder="Add Telephone"
+                    onChange={(e) => setTelephone(e.target.value)}
+
+                />
+
+                <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Add Email"
+                    onChange={(e) => setEmail(e.target.value)}
+
+                />
+                
+                <input
+                    type="submit"
+                    className="btn btn-dark btn-block"
+                    value="Submit"
+                 />
+
+            </form>
+        </div>
+    )
+}
+
+export default AutoAddress;
