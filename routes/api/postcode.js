ccf8ae13f5365d02a68b89b36ac7c33068ecf62a
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-21 18:40:47 +0100

    added two routes for api to get postcode autocomplete and to get address possibilities

---
+++ routes/api/postcode.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const axios = require('axios');
+const config = require('config');
+const router = express.Router();
+
+
+// ALL USERS
+router.get('/:postcode', async (req, res) => {
+try {
+
+ 
+    const postcodeRes = await axios.get(
+        `https://api.postcodes.io/postcodes/${req.params.postcode}/autocomplete`
+      );
+      return res.json(postcodeRes.data);
+    } catch (err) {
+
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Postcode found' });
+    }
+  });
+
+//GET ADDRESS FROM POSTCODE
+
+router.get('/address/:postcode', async (req, res) => {
+  try {
+
+ 
+    const addressRes = await axios.get(
+      `https://api.getAddress.io/find/${req.params.postcode}?api-key=${config.get("getAddressIoApiKey")}`
+      );
+      return res.json(addressRes.data);
+    } catch (err) {
+
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Postcode found' });
+    }
+  });
+
+
+
+
+  module.exports = router;
