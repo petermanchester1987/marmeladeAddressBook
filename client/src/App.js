221c0558841ab0bdc7087b037e913f4835c2f53a
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-21 19:19:33 +0100

    adding components to then build out

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Fragment } from 'react';
 import './App.css';
-import AddPostcode from './components/address/AddPostcode';
-import AddAddress from './components/address/AddAddress';
+import AddPostcode from './components/addAddress/AddPostcode';
+import AddAddress from './components/addAddress/AddAddress';
 import Navbar from './components/layout/Navbar';
 
 
@@ -11,8 +11,12 @@ const App = () => {
     <Fragment>
       <Navbar />
       <div className="container">
+        <h1>Add An Address</h1>
         <AddPostcode />
         <AddAddress />
+      </div>
+      <div className="container">
+        <AddressList />
       </div>
     </Fragment>
   );
---
+++ client/src/components/addAddress/AddPostcode.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AddPostcode = () => {
+    return (
+        <div>
+           <input
+          type="text"
+          name="quickform"
+          list="postcode-array"
+          placeholder="Enter Postcode"
+          
+        /> 
+        </div>
+    )
+}
+
+export default AddPostcode
