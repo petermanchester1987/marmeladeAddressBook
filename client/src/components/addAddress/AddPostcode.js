d491626a49b64ceff3f8ef19256886f9782da836
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-21 21:53:09 +0100

    removed useState

--- client/src/components/addAddress/AddPostcode.js
+++ client/src/components/addAddress/AddPostcode.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useCallback, useContext } from 'react';
 import AddressContext from '../../context/address/addressContext';
 import { debounce } from 'throttle-debounce';
 
